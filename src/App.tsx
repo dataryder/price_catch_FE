d3be5858269669dd4b0894609d67fb58f3723c82
Justin Koay <justintzeji@gmail.com>
2025-05-17 01:22:10 +0800

    fix: theme persist across page reloads

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ import ItemDetailsWrapper from './pages/ItemDetailsWrapper';
 import HomePage from './pages/Home';
 import FooterBar from './components/Footer';
 import CategoryPage from './pages/CategoryCatalog';
+import { useTheme } from "@govtechmy/myds-react/hooks";
+
+
 
 function App() {
-
+  const { theme, setTheme } = useTheme();
+  setTheme(localStorage.getItem("theme") || "light")
+  console.log(theme);
   return (
     <div className="mx-auto w-md md:w-xl lg:w-6xl min-h-screen">
       <div className='mx-auto min-h-screen flex flex-col flex-grow'>
