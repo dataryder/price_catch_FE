b84c305ef266895b780ab78ac4587b2c0d06a327
Justin Koay <justintzeji@gmail.com>
2025-05-16 21:24:32 +0800

    fix: overflow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import HeroPattern from '../components/HeroPattern';
 
 const HomePage: React.FC = () => {
 	return (
-		<div className="relative flex gap-4 md:gap-6 py-2 px-6 md:py-4 md:px-8 bg-gradient-to-b from-bg-success-700 via-bg-success-300 to-bg-bg-white min-h-[200px] md:min-h-[300px] justify-center flex flex-col overflow-hidden">
+		<div className="relative flex gap-4 md:gap-6 py-2 px-6 md:py-4 md:px-8 bg-gradient-to-b from-bg-success-700 via-bg-success-300 to-bg-bg-white min-h-[200px] md:min-h-[300px] justify-center flex flex-col">
 			<HeroPattern className="absolute top-0 left-1/2 transform -translate-x-1/2" />
 			<div className='container mx-auto 2xl:px-40 grid grid-col gap-4'>
 				<h1 className='text-xl md:text-2xl text-center text-txt-white font-bold uppercase tracking-[0.2em] md:tracking-[0.4em]'>PRICES GOIN CRAZY?</h1>
