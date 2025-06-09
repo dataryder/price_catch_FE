d02db24587bc755d5903812638e71a78b3797754
Justin Koay <justintzeji@gmail.com>
2025-06-09 14:41:10 +0800

    fix:: unused var

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SearchResultItem, ItemDetailsInput, ItemMetadata, ItemLatest, ItemPriceHistory } from '../types';
+import { ItemDetailsInput, ItemMetadata, ItemLatest, ItemPriceHistory } from '../types';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/';
 const apiClient = axios.create({
